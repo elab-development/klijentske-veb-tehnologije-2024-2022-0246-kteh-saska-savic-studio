3f470c80f586b15020fa6e8bedff75735798a8f7
Saska Savic <saskasavic1809@gmail.com>
2024-08-11 21:05:00 +0000

    public/img,data +application router skeleton,style

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import NoPage from './pages/NoPage'
+import Cart from './pages/Cart'
+import ProductsPage from './pages/ProductsPage'
+import { useEffect, useState } from 'react'
+import { User } from './models/userModel'
+import { Product } from './models/productModel'
+import Login from './pages/Login'
+//import {useFetch} from './hooks/useFetch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // promenljive odozdo
+  const [user, setUser] = useState<User | Record<string, never>>({firstName: "Site", lastName: "User"})
+
+  //const [cartNum, setCartNum] = useState(0);
+  //const [cartProducts, setCartProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+      //treba proveriti sesiju na serveru - OVDE KAO DA JE DOBIJEN KORISNIK
+      setUser({firstName: "Site", lastName: "User"})
+  }, [])
+
+  //try {
+  useEffect(() => {
+      //const {data} = useFetch('http://localhost:5173/data/products.json')
+      const fetchData = async () => {
+        const response = await fetch('/data/products.json')
+        const data = await response.json()
+        console.log('data: ', data)
+        setProducts(data.products.map((el: unknown) => {
+          const elem = el as Product
+          return new Product(elem.productid, elem.name, elem.description, elem.price, elem.image, elem.category, elem.recommended)
+      }))
+
+      }
+      fetchData()
+  },[])
+      
+
+  const router = createBrowserRouter(
+    createRoutesFromElements([
+      <Route path="/" element={<Layout user={user} />}>
+        <Route index element={<Home products={products}/>} />
+        <Route path="/products" element={<ProductsPage products={products}/>} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    ])
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
   )
 }
 
